46ff2a98905a6812b6c70e4e951011bbcb056064
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2021-11-08 17:51:23 +0100

    barebones
    it's pratically done but i'm looking for stuff to improve

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const Home = () => {
+  return (
+    <main>
+      <h1>One Page React Template</h1>
+      <p className='home__p'>
+        the better create react app
+      </p>
+    </main>
+  );
+};
+
+export default Home;
