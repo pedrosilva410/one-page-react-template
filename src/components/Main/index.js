77094a0a048b1455a4967eae78024f533c399455
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2021-11-21 19:08:56 +0100

    21/11

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,7 +4,13 @@ import './styles.css';
 const Main = () => {
   return (
     <main>
-      ola
+      <h1 className='mainH1'>
+        One Page Template
+      </h1>
+      <p className='mainP'>
+        The purpose of this project is to be reused for react apps that will only need one page!
+        Feel free to use it as often as you want!
+      </p>
     </main>
   );
 };
